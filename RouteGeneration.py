c0b07c334d42d1418be157ae7c86fe943c61219e
mykobob <michael.eagle@utexas.edu>
2017-10-03 19:42:19 -0500

    updated number of clusters

--- RouteGeneration.py
+++ RouteGeneration.py
@@ -13,13 +13,13 @@ class RouteGeneration:
         self.locations = self.parse_input_data()
 
     def parse_input_data(self):
-        num_locations = int(input().strip())
+        self.num_locations = int(input().strip())
         locations_dict = {}
-        for i in range(0, num_locations):
+        for i in range(0, self.num_locations):
             name = input().strip()
             locations_dict[name] = Location(name)
 
-        for i in range(0, num_locations * (num_locations - 1) // 2):
+        for i in range(0, self.num_locations * (self.num_locations - 1) // 2):
             data = input().strip().split(',')
             src = locations_dict[data[0]]
             dest = locations_dict[data[1]]
@@ -33,17 +33,25 @@ class RouteGeneration:
         starting_loc = defaultdict(int)
         names = set()
         for route in cluster:
-            starting_loc[route.start_loc()] += 1
+            starting_loc[route.start_loc().name] += 1
             names.add(route.start_loc().name)
-        return len(names) == 8 and not any(starting_count < 2 for starting_count in starting_loc.values()) 
+
+        return len(names) == self.num_locations and not any(starting_count < 2 for starting_count in starting_loc.values()) 
 
     def matching_locations(self, valid_routes, route_in_question):
         return any(route_in_question[i] == route[i] and route_in_question[i + 1] == route[i + 1] for route in valid_routes for i in range(route.length - 1))
 
+    def has_two_of_each_loc(self, selected_arr):
+        two_dict = defaultdict(int)
+        for route in selected_arr:
+            two_dict[route.start_loc().name] += 1
+
+        return all(count == 2 for count in two_dict.values())
+
     def get_num_routes(self, num_routes, cluster):
         used = [False] * len(cluster)
         selected = []
-        while len(selected) < num_routes:
+        while len(selected) < num_routes and self.has_two_of_each_loc(selected):
             index = randrange(len(cluster))
             while used[index] or self.matching_locations(selected, cluster[index]):
                 index = randrange(len(cluster))
@@ -56,29 +64,37 @@ class RouteGeneration:
         good = []
         for cluster in clusters:
             valid_routes = self.get_num_routes(num_routes, cluster)
-            if valid_routes is not None:
-                good.append(valid_routes)
+            good.append(valid_routes)
         return good
 
     def apply_filters(self, num_routes, clusters):
         # There needs to be enough things in the cluster to be use for routes
         clusters = list(filter(lambda cluster: len(cluster) >= num_routes, clusters))
+        print('    finished basic filtering')
 
         # There can only exist two routes with the same starting place
         clusters = list(filter(self.two_starting_locs, clusters))
+        print('    finished same source nodes')
 
         # No two routes can have a location in the same position and lead to the next same location
         # Select 16 routes in each cluster that satisfy above condition
         clusters = self.get_good_clusters(num_routes, clusters)
+        print('    get good clusters')
 
         return clusters
 
 
     def generate_routes(self, num_routes):
         all_perms = [Route(x) for x in itertools.permutations(self.locations)] 
-        clusters = KMean(all_perms).cluster()
+        print('Finished generating all permutations')
 
-        return self.apply_filters(num_routes, clusters)
+        clusters = KMean(all_perms).cluster()
+        print('Finished clustering')
+
+        filtered_data = self.apply_filters(num_routes, clusters)
+        print('Finished filters')
+
+        return filtered_data
 
     def solve(self):
         route_perms = self.generate_routes(16)
--- kmeans.py
+++ kmeans.py
@@ -1,5 +1,6 @@
 from sklearn.cluster import KMeans
 
+from collections import defaultdict
 import numpy as np
 
 class KMean:
@@ -9,27 +10,32 @@ class KMean:
         self.epsilon = 1e-6
 
     def create_mapping(self):
+        highest_key_map = {}
         self.dist_map = {}
         for route in self.data:
             length = route.length 
-            while length in self.dist_map:
-                length += self.epsilon
-            self.dist_map[length] = route
+
+            largest_length = highest_key_map.get(length)
+            if largest_length is None:
+                largest_length = length + self.epsilon
+            else:
+                largest_length += self.epsilon
+            highest_key_map[length] = largest_length
+
+            self.dist_map[largest_length] = route
         return np.array(list(self.dist_map.keys()))
 
     def cluster(self):
-        num_clusters = 50
+        num_clusters = 9
         clusterable_data = self.create_mapping()
         kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(clusterable_data.reshape(-1,1))
         labels = kmeans.labels_
 
-        tmp_dict = {}
+        tmp_dict = defaultdict(list)
         for i in range(0, len(labels)):
             label = labels[i]
             key = clusterable_data[i]
             route = self.dist_map[key]
-            if label not in tmp_dict:
-                tmp_dict[label] = []
             tmp_dict[label].append(route)
 
         return list(tmp_dict.values())
